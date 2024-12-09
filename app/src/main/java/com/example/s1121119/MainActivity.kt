df1c09a8c323cf1c0253e1877d2954518287a199
v0_劉伊宸 <emilyliu0227@gmail.com>
2024-12-09 16:52:33 +0800

    v4_劉伊宸a

--- app/src/main/java/com/example/s1121119/MainActivity.kt
+++ app/src/main/java/com/example/s1121119/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +26,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import com.example.s1121119.ui.theme.S1121119Theme
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.ui.geometry.Offset
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -89,6 +95,12 @@ fun GameScreen(modifier: Modifier = Modifier) {
         ) {
             Text(text = "結束App")
 
+            Image(
+                painter = painterResource(id = R.drawable.maria2),
+                contentDescription = "maria2",
+                modifier = Modifier.padding(vertical = 30.dp)
+            )
+
         }
     }
 }
@@ -99,3 +111,50 @@ fun DefaultPreview() {
         GameScreen()
     }
 }
+
+@Composable
+fun Tap() {
+    var msg by remember { mutableStateOf("TAP相關手勢實例") }
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset.Zero) }
+    var PU = arrayListOf(R.drawable.maria2, R.drawable.maria2,
+        R.drawable.maria2, R.drawable.maria2,
+        R.drawable.maria2, R.drawable.maria2)
+    var Number by remember { mutableStateOf(0) }
+
+
+    Column {
+        Text("\n" + msg)
+
+        Image(
+            painter = painterResource(id = PU[Number]),
+            contentDescription = "瑪麗亞之美",
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGesturesAfterLongPress(
+                        onDrag = { change, dragAmount -> offset2+=dragAmount},
+                        onDragStart = {
+                            offset1 = it
+                            offset2 = it
+                                      },
+                        onDragEnd = {if (offset2.x >= offset1.x){
+                            msg = "長按後向右拖曳"
+                            Number ++
+                            if (Number>5){Number=0}
+                        }
+                        else{
+                            msg = "長按後向左拖曳"
+                            Number --
+                            if (Number<0){Number=5}
+                        }
+                        },
+
+                    )
+
+                }
+        )
+
+}
+}
+
