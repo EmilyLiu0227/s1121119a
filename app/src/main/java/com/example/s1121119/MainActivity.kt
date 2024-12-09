b86e37b9cb854c48e9d2cd1e13448b11ac641d54
v0_劉伊宸 <emilyliu0227@gmail.com>
2024-12-09 15:00:01 +0800

    v0_劉伊宸

--- app/src/main/java/com/example/s1121119/MainActivity.kt
+++ app/src/main/java/com/example/s1121119/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.s1121119.ui.theme.S1121119Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +35,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+        var count = 0
+        Text(text = count.toString(),
+            fontSize = 50.sp)
+
+    }
 }
 
 @Preview(showBackground = true)
